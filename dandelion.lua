c1003aef4919c562ed69510c62fb16709d46674f
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2013-03-29 08:58:09 -0300

    Fixed a few formatting things.

--- dandelion.lua
+++ dandelion.lua
@@ -18,11 +18,11 @@
 
 -- variable that holds the name of all valid elements
 local dandelionElements = { "id",
-							"name",
-							"group",
-							"author",
-							"description",
-							"expects" }
+	"name",
+	"group",
+	"author",
+	"description",
+	"expects" }
 
 --- Draws the application logo in the terminal.
 -- This function simply draws an ASCII logo in the terminal.
@@ -31,8 +31,8 @@ local dandelionElements = { "id",
 -- people will come after me when code starts to fail
 -- miserably! :)
 local function drawLogo()
-	print("    _              _     _ _")         
-	print(" __| |__ _ _ _  __| |___| (_)___ _ _") 
+	print("    _              _     _ _")
+	print(" __| |__ _ _ _  __| |___| (_)___ _ _")
 	print("/ _` / _` | ' \\/ _` / -_) | / _ \\ ' \\ ")
 	print("\\__,_\\__,_|_||_\\__,_\\___|_|_\\___/_||_|")
 	print("\nCopyright 2013, The LaTeX3 Project")
@@ -69,19 +69,19 @@ local function validEntries(t)
 
 	-- iterate through all elements
 	for i, v in pairs(t) do
-	
+
 		-- the 'expects' key is relaxed on empty values,
 		-- so let's ignore it
 		if i ~= "expects" then
-		
+
 			-- if the values are empty,
 			-- return false
 			if trim(v) == "" then
 				return false
-			end		
+			end
 		end
 	end
-	
+
 	-- everything is okay
 	return true
 end
@@ -104,7 +104,7 @@ local function contains(a, t)
 			return true
 		end
 	end
-	
+
 	-- nothing was found
 	return false
 end
@@ -119,17 +119,17 @@ local function difference(tableOne, tableTwo)
 
 	-- temporary variable
 	local tableThree = {}
-	
+
 	-- iterate through all elements in table 1
 	for _, element in ipairs(tableOne) do
-	
+
 		-- for every element a from table 1 not
 		-- in table 2, insert it into table 3
 		if not contains(element, tableTwo) then
 			table.insert(tableThree, element)
 		end
 	end
-	
+
 	-- return the difference between
 	-- table 1 and 2
 	return tableThree
@@ -149,144 +149,144 @@ local function extractMetadataBlocks(filename)
 	-- create a file handler and open the
 	-- file accordingly
 	local fileHandler = io.open(filename,"r")
-	
-	-- if something bad happens, go to the 
+
+	-- if something bad happens, go to the
 	-- 'else' branch, end script and live
 	-- happily ever after
 	if fileHandler then
-	
+
 		-- flag to enable or disable parsing of commented
 		-- lines, according to the context
 		local grabber = false
-		
+
 		-- flag to indicate if the current multine parsing
 		-- is valid, of course, you need to have a single
 		-- line argument first
 		local acceptMultine = false
-		
+
 		-- line counter
 		local lineCounter = 1
-		
+
 		-- variable to hold the key
 		-- from metadata
 		local key
-		
+
 		-- mapping table
 		local mapping = nil
-		
+
 		-- full mappings table
 		local mappings = {}
-		
+
 		-- index for the mappings table
 		local mappingIndex = 1
-		
+
 		-- flag for an empty value
 		-- for a single entry
 		local emptyValue = false
-		
-	
+
+
 		-- read every single line of the provided
 		-- file and look for the test patterns
 		for currentLine in fileHandler:lines() do
-		
+
 			-- look for the test pattern in the
 			-- source code comments
 			if string.find(currentLine, "^%s*%%%s*!test$") then
-			
+
 				-- let's reset our mapping table
 				mapping = {}
-							
+
 				-- we are now in a potential test metadata block,
 				-- so let's enable the flag
-				grabber = true 
-				
-			-- not a test line, but we can also
-			-- look for test metadata block, if
-			-- the flag is enabled
+				grabber = true
+
+				-- not a test line, but we can also
+				-- look for test metadata block, if
+				-- the flag is enabled
 			else
-			
+
 				-- apparently, we are in a test metadata block!
 				if grabber then
-				
+
 					-- extract all subsequent comment lines until a
 					-- non commented line appears in the processor
 					if string.find(currentLine, "^%s*%%") then
-					
+
 						-- create a new local variable
 						-- to hold the current entry
 						local entry
-					
+
 						-- get everything after the comment part
 						entry = string.match(currentLine, '^%s*%%(.+)$')
-						
+
 						-- if we have a nil value, this is not
 						-- good! The test metadata block requires
 						-- no empty comment lines.
 						if entry == nil then
-						
+
 							-- message
 							print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 							print("Inside the test metadata block, we don't expect")
 							print("commented lines without a proper description,")
 							print("that is, empty comments. Stopping execution.")
-							
+
 							-- stop, hammertime!
 							os.exit()
-						
+
 						end
-						
+
 						-- check if we are handling multiline values
 						if string.find(entry, "^%%+") then
-						
+
 							-- we have a special line here,
 							-- so let's find out what's going
 							-- on in this code
-							
+
 							-- let's see if the scope is valid, that
 							-- is, we need to have a previous line
 							-- with a key
 							if acceptMultine == false then
-							
+
 								-- print message
 								print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 								print("You can't have a multiline entry without a previous")
 								print("single entry with the proper key. Stopping execution.")
-								
+
 								-- spam, spam, spam, spam,
 								-- lovely spam, wonderful
 								-- spam!
 								os.exit()
-							
+
 							end
-							
+
 							-- let's define a new variable
 							-- to set the type of this
 							-- multiline block
 							local type
-							
+
 							-- perform the matching pattern
 							-- expects something
 							type, entry = string.match(entry, '^(%s*%%+%s)(.*)$')
-							
+
 							-- if type is nil, we have a malformed
 							-- line not complying with our spec
 							if type == nil then
-							
+
 								-- alert the user and provide the line
 								-- of the offending code
 								print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 								print("I require at least one space separating the comment")
 								print("symbol and the content. Stopping execution.")
-								
+
 								-- kill it with fire!
 								os.exit()
-							
+
 							end
-							
+
 							-- so we have a valid type, woohoo!
-							-- Let's trim it for the greater good							
+							-- Let's trim it for the greater good
 							type = trim(type)
-							
+
 							-- we have two types of multiline
 							-- metadata comments:
 							--
@@ -296,152 +296,152 @@ local function extractMetadataBlocks(filename)
 							-- 2: linebreaks are preserved, with
 							--    the proper line ending, named
 							--    'codestring'
-							
+
 							local lineEnding
-							
+
 							-- we have a docstring
 							if #type == 1 then
-							
+
 								-- add single space
 								lineEnding = " "
-							
-							-- we have a codestring
+
+								-- we have a codestring
 							elseif #type == 2 then
-							
+
 								-- add a linebreak
 								lineEnding = "\n"
-							
-							-- none of the above,
-							-- raise error						
+
+								-- none of the above,
+								-- raise error
 							else
-							
+
 								-- print message
 								print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 								print("The number of extra comment symbols is invalid.")
 								print("Stopping execution.")
-								
+
 								-- fire at will!
 								os.exit()
-							
+
 							end
-							
+
 							-- if we are in a multiline environment,
 							-- we expect to fetch values, so let's
 							-- disable the flag
 							emptyValue = false
-							
+
 							-- let's concatenate the values from the previous
 							-- lines, since we are in a multiline segment
 							entry = mapping[key] .. lineEnding .. entry
-							
+
 							-- if docstring, simply trim
 							-- the leading and trailing
 							-- spaces
 							if #type == 1 then
 								entry = trim(entry)
-								
-							-- we have a codestring, then
-							-- let's trim the leading and
-							-- trailing newlines
+
+								-- we have a codestring, then
+								-- let's trim the leading and
+								-- trailing newlines
 							else
 								entry = trimNewline(entry)
 							end
-							
+
 							-- add entry to the mappings
-							mapping[key] = entry 
+							mapping[key] = entry
 							mappings[mappingIndex] = mapping
-													
-						-- it seems we are handling a
-						-- normal line in here	
+
+							-- it seems we are handling a
+							-- normal line in here
 						else
-						
+
 							-- normal line
-							
+
 							-- check if there was a previous single
 							-- line with an empty value
 							if emptyValue then
-							
+
 								-- raise error, print message
 								print("I'm sorry, but there's an invalid entry at line " .. (lineCounter - 1) .. ".")
 								print("A key must always be associated to a value.")
 								print("Either set a value or add a multiline comment.")
 								print("Stopping execution.")
-								
+
 								-- lalala can't hear you lalala
 								os.exit()
-								
+
 							end
-							
+
 							-- we can now accept subsequent
 							-- multiline entries
 							acceptMultine = true
 
 							-- let's get both key and the value
 							key, entry = string.match(entry, '^%s*(%w+)%s*:(.*)$')
-							
+
 							-- a key is required, so if it doesn't comply
 							-- with the pattern, the value will be nil
 							-- and an error will be raised
 							if key == nil then
-							
+
 								-- print message
 								print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 								print("It appears there is no key, or the existing one is")
 								print("invalid. Stopping execution.")
-								
+
 								-- I like ice cream
 								os.exit()
-								
+
 							end
-							
+
 							-- we need to ensure we have a
 							-- valid key, otherwise stop
 							-- the press!
 							if not validKey(key) then
-							
+
 								-- print message
 								print("I'm sorry, but there's an invalid entry at line " .. lineCounter .. ".")
 								print("It appears the '" .. key .. "' is invalid. Please")
 								print("fix it before proceeding. Stopping execution.")
-								
+
 								-- can't read ma, can't read ma,
 								-- no you can't read ma poker face!
 								os.exit()
-							
+
 							end
-							
+
 							-- if the current value is not
 							-- empty, set flag to false
 							-- and trim spaces
 							if entry ~= "" then
-								
-								-- set flag to false							
+
+								-- set flag to false
 								emptyValue = false
-								
+
 								-- trim spaces
-								entry = trim(entry)								
-							
-							-- we have an empty value
+								entry = trim(entry)
+
+								-- we have an empty value
 							else
-							
+
 								-- set flag to true,
 								-- and now we expect
 								-- multiline
 								emptyValue = true
 							end
-							
+
 							-- key is already trimmed because
 							-- of the matching pattern, so let's
 							-- simply add things here
 							mapping[key] = entry
 							mappings[mappingIndex] = mapping
-						
+
 						end
-					
-					-- no commented line, so let's disable
-					-- the block extraction
+
+						-- no commented line, so let's disable
+						-- the block extraction
 					else
-					
+
 						-- check if there was a previous single
 						-- line with an empty value
 						if emptyValue then
@@ -451,111 +451,111 @@ local function extractMetadataBlocks(filename)
 							print("A key must always be associated to a value.")
 							print("Either set a value or add a multiline comment.")
 							print("Stopping execution.")
-								
+
 							-- lalala I can't hear
 							-- you lalala
 							os.exit()
 
 						end
-					
+
 						-- false, false, false
 						grabber = false
 						acceptMultine = false
-					
+
 						-- increment mapping index
 						mappingIndex = mappingIndex + 1
-					
+
 					end
-					
+
 				end
-				
+
 			end
-		
+
 			-- increment line counter
 			lineCounter = lineCounter + 1
-		
+
 		end
-	
+
 		-- close handler, everything
 		-- went fine
 		fileHandler:close()
-		
+
 		-- now, let's do some sanity check in order to
 		-- ensure our table of mappings is correct
-		
+
 		-- list of test id's,
 		-- empty at first
 		local idList = {}
-		
+
 		-- now let's browse our mappings table
 		-- looking for all tests we found
 		for _, v in ipairs(mappings) do
-		
+
 			-- our temporary table to hold the
 			-- current test keys
 			local keys = {}
-			
+
 			-- for every key in the current
 			-- test spec, add it to the list
 			for j, _ in pairs(v) do
-				table.insert(keys, j)				
+				table.insert(keys, j)
 			end
-			
+
 			-- if we have missing elements, the
 			-- execution has to stop
 			if #difference(dandelionElements, keys) ~= 0 then
-			
+
 				-- print message
 				print("I'm sorry, but there are some elements missing from one")
 				print("of your tests. Please make sure the test spec has all the")
 				print("required elements. Stopping execution.")
-			
+
 				-- pew, pew, pew!
 				os.exit()
-			
+
 			end
-			
+
 			-- now let's search for invalid
 			-- entries, that is, empty values
 			if not validEntries(v) then
-			
+
 				-- print message
 				print("I'm sorry, but there are some elements with empty values.")
 				print("Please make sure the test spec has all the required elements")
 				print("and their corresponding values. Stopping execution.")
-				
+
 				-- set fire to the rain!
 				os.exit()
-				
+
 			end
-			
+
 			-- if the current test id is already defined,
 			-- an error has to be raised!
 			if contains(v["id"], idList) then
-			
+
 				-- print message
 				print("I'm sorry, but the test ID '" .. v["id"] .. "' is already defined")
 				print("in your file. Please rename it. Stopping execution.")
-			
+
 				-- to exit or not to exit,
 				-- that's the question
 				os.exit()
-				
-			-- new id, let's add it to the list of
-			-- test id's
+
+				-- new id, let's add it to the list of
+				-- test id's
 			else
 				table.insert(idList, v["id"])
 			end
-			
+
 		end
-							
-	-- Bad dog, bad dog!
+
+		-- Bad dog, bad dog!
 	else
-	
+
 		-- print a very polite message
 		print("File '" .. filename .. "' does not exist or is unavailable.")
 		print("Interrupting script, have a nice day.")
-		
+
 		-- BOOM headshot!
 		os.exit()
 	end
